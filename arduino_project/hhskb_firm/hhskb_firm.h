5994b836e242a86bd8d24d542b66a2d94eec6d9f
yishida <yishida@jeol.co.jp>
2017-05-31 16:15:27 +0900

    リファクタリング かなりスッキリ

---
+++ arduino_project/hhskb_firm/hhskb_firm.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// hhskb_firmで使う一般的な型、定数定義
+
+enum LorR
+{
+	Left,
+	Right,
+};
+static const int OFF = 1;
+static const int ON = 0;    // 今回の回路はアクティブロー
+static const int NO_ASMBL = -1;
+
+// 特殊文字宣言
+static const char TAB = 0x09;
+static const char SPC = 0x20;
+static const int Fn = 0xf0;   // 適当
+static const int PRTSC = 0xce;  // print screen?
+
+// キー配列の最大数
+#define ROWMAX  5
+#define COLMAX  8
+static const int SUM = 40;
+
--- arduino_project/hhskb_firm/hhskb_firm.ino
+++ arduino_project/hhskb_firm/hhskb_firm.ino
@@ -1,68 +1,60 @@
 #include <Keyboard.h>
+#include "hhskb_firm.h"
 #include "right_firm.h"
 #include "left_firm.h"
-
-#define ROWMAX  5
-#define COLMAX  8
-static const int SUM = 40;
-
-// 一般的な定義
-static const int OFF = 1;
-static const int ON = 0;    // 今回の回路はアクティブロー
-static const int NO_ASMBL = -1;
-
-// 特殊文字宣言
-static const char TAB = 0x09;
-static const char SPC = 0x20;
-static const int Fn = 0xf0;   // 適当
-static const int PRTSC = 0xce;  // print screen?
-
-// ファンクションキー関連
-static bool IsFnEnable = false;
 
 // 右手通常文字の配置定義
 static int RSymbol[ROWMAX][COLMAX] =
 {
-  { '7', '8', '9', '0', '-', '=', '\\', '`' },
-  { 'y', 'u', 'i', 'o', 'p', '[', ']', KEY_DELETE },
-  { 'h', 'j', 'k', 'l', ';', '\'', KEY_RETURN, NO_ASMBL },
-  { 'b', 'n', 'm', ',', '.', '/', KEY_RIGHT_SHIFT, Fn },
-  { SPC, KEY_RIGHT_ALT, KEY_RIGHT_CTRL, NO_ASMBL, KEY_RIGHT_GUI, NO_ASMBL, NO_ASMBL, NO_ASMBL }
+	{ '7', '8', '9', '0', '-', '=', '\\', '`' },
+	{ 'y', 'u', 'i', 'o', 'p', '[', ']', KEY_DELETE },
+	{ 'h', 'j', 'k', 'l', ';', '\'', KEY_RETURN, NO_ASMBL },
+	{ 'b', 'n', 'm', ',', '.', '/', KEY_RIGHT_SHIFT, Fn },
+	{ SPC, KEY_RIGHT_ALT, KEY_RIGHT_CTRL, NO_ASMBL, KEY_RIGHT_GUI, NO_ASMBL, NO_ASMBL, NO_ASMBL }
 };
 
-// 右手用ファンクション押下時シンボル
+// 右手用ファンクション押下時シンボル 
+// 通常側でFnを定義している箇所はここで変更出来ません。
 static int RFnSymbol[ROWMAX][COLMAX] =
 {
-  { KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_INSERT, KEY_DELETE },
-  { 0, 0, PRTSC, 0, 0, KEY_UP_ARROW, 0, KEY_BACKSPACE },
-  { 0, 0, KEY_HOME, KEY_PAGE_UP, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, 0, NO_ASMBL },
-  { 0, 0, 0, KEY_END, KEY_PAGE_DOWN, KEY_DOWN_ARROW, KEY_RIGHT_SHIFT, Fn },
-  { SPC, KEY_RIGHT_ALT, KEY_RIGHT_CTRL, NO_ASMBL, KEY_RIGHT_GUI, NO_ASMBL, NO_ASMBL, NO_ASMBL }
+	{ KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_INSERT, KEY_DELETE },
+	{ 0, 0, PRTSC, 0, 0, KEY_UP_ARROW, 0, KEY_BACKSPACE },
+	{ 0, 0, KEY_HOME, KEY_PAGE_UP, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, 0, NO_ASMBL },
+	{ 0, 0, 0, KEY_END, KEY_PAGE_DOWN, KEY_DOWN_ARROW, KEY_RIGHT_SHIFT, Fn },
+	{ SPC, KEY_RIGHT_ALT, KEY_RIGHT_CTRL, NO_ASMBL, KEY_RIGHT_GUI, NO_ASMBL, NO_ASMBL, NO_ASMBL }
 };
-
-// ひとつ前のパース結果を格納する変数
-static char OldRKey[ROWMAX][COLMAX];
-static char OldLKey[ROWMAX][COLMAX];
 
 // 左手用シンボル
 static int LSymbol[ROWMAX][COLMAX] =
 {
-  { KEY_ESC, '1', '2', '3', '4', '5', '6', NO_ASMBL },
-  { TAB, 'q', 'w', 'e', 'r', 't', NO_ASMBL, NO_ASMBL },
-  { KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g', NO_ASMBL, NO_ASMBL },
-  { KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', NO_ASMBL, NO_ASMBL },
-  { NO_ASMBL, NO_ASMBL, KEY_LEFT_GUI, 0, KEY_LEFT_ALT, KEY_LEFT_CTRL, NO_ASMBL, NO_ASMBL }
+	{ KEY_ESC, '1', '2', '3', '4', '5', '6', NO_ASMBL },
+	{ TAB, 'q', 'w', 'e', 'r', 't', NO_ASMBL, NO_ASMBL },
+	{ KEY_LEFT_CTRL, 'a', 's', 'd', 'f', 'g', NO_ASMBL, NO_ASMBL },
+	{ KEY_LEFT_SHIFT, 'z', 'x', 'c', 'v', 'b', NO_ASMBL, NO_ASMBL },
+	{ NO_ASMBL, NO_ASMBL, KEY_LEFT_GUI, 0, KEY_LEFT_ALT, KEY_LEFT_CTRL, NO_ASMBL, NO_ASMBL }
 };
 
 // 左手用Fn押下時シンボル
+// 通常側でFnを定義している箇所はここで変更出来ません。
 static int LFnSymbol[ROWMAX][COLMAX] =
 {
-  { KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, NO_ASMBL },
-  { TAB, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
-  { KEY_LEFT_CTRL, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
-  { KEY_LEFT_SHIFT, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
-  { NO_ASMBL, NO_ASMBL, KEY_LEFT_GUI, 0, KEY_LEFT_ALT, KEY_LEFT_CTRL, NO_ASMBL, NO_ASMBL }
+	{ KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, NO_ASMBL },
+	{ TAB, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
+	{ KEY_LEFT_CTRL, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
+	{ KEY_LEFT_SHIFT, 0, 0, 0, 0, 0, NO_ASMBL, NO_ASMBL },
+	{ NO_ASMBL, NO_ASMBL, KEY_LEFT_GUI, 0, KEY_LEFT_ALT, KEY_LEFT_CTRL, NO_ASMBL, NO_ASMBL }
 };
+
+// Fnキー状態
+static bool IsFnEnable = false;
+
+// 今のキー状態
+static char RKey[ROWMAX][COLMAX];
+static char LKey[ROWMAX][COLMAX];
+
+// ひとつ前のパース結果を格納する変数
+static char OldRKey[ROWMAX][COLMAX];
+static char OldLKey[ROWMAX][COLMAX];
 
 void setup() {
 	// put your setup code here, to run once:
@@ -77,102 +69,135 @@ void setup() {
 
 void loop() {
 	// put your main code here, to run repeatedly:
-	char RKey[ROWMAX][COLMAX];
+
 	memset(RKey, (char)OFF, SUM);
 	ParseRightKey(RKey);
-	if (keyboardAction(RKey, OldRKey, RSymbol, RFnSymbol) == 1)
-	{
-		releaseFuncKeyAction(OldRKey, RFnSymbol);
-		releaseFuncKeyAction(OldLKey, LFnSymbol);
-		memset(OldRKey, (char)OFF, SUM);
-		memset(OldLKey, (char)OFF, SUM);
-	}
-	else
-	{
-		memcpy(OldRKey, RKey, SUM);
-	}
-
-	char LKey[ROWMAX][COLMAX];
 	memset(LKey, (char)OFF, SUM);
 	ParseLeftKey(LKey);
-	if (keyboardAction(LKey, OldLKey, LSymbol, LFnSymbol) == 1)
-	{
-		releaseFuncKeyAction(OldRKey, RFnSymbol);
-		releaseFuncKeyAction(OldLKey, LFnSymbol);
-		memset(OldRKey, (char)OFF, SUM);
-		memset(OldLKey, (char)OFF, SUM);
-	}
-	else
-	{
-		memcpy(OldLKey, LKey, SUM);
-	}
+
+	keyboardAction(Right);
+	memcpy(OldRKey, RKey, SUM);
+	keyboardAction(Left);
+	memcpy(OldLKey, LKey, SUM);
 }
 
-// シンボルの検索、キーコードのプレス、リリースコードの発射を行います。
-// 戻り値 
-//  0:正常終了。
-//  1:ファンクションがリリースされた事を示す終了値。途中で処理が終了しています。
-int keyboardAction(char Parsed[][COLMAX], char OldParsed[][COLMAX], int Symbol[][COLMAX], int FnSymbol[][COLMAX])
+// 左右どちらかのモジュールに対してシンボルの検索、キーコードのプレス/リリースの発射を行います。
+static void keyboardAction(LorR lr)
 {
+	int tgt = 0;
 	for (int row = 0; row < ROWMAX; row++)
 	{
 		for (int col = 0; col < COLMAX; col++)
 		{
-			if ((Parsed[row][col] == ON) && (OldParsed[row][col] == OFF))
+			if (isTurnOn(row, col, lr))
 			{
-				if (Symbol[row][col] == Fn)
+				if ((tgt = getSymbol(row, col, lr, IsFnEnable)) == Fn)
 				{
 					IsFnEnable = true;
 					TurnOnStatusLed(2);
 				}
-				if (IsFnEnable)
-				{
-					Keyboard.press(FnSymbol[row][col]);
-				}
 				else
 				{
-					Keyboard.press(Symbol[row][col]);
+					Keyboard.press(tgt);
 				}
 			}
-			else if ((Parsed[row][col] == OFF) && (OldParsed[row][col] == ON))
-			{
-				if (Symbol[row][col] == Fn)
+			else if (isTurnOff(row, col, lr))
+			{			
+				if ((tgt = getSymbol(row, col, lr, IsFnEnable)) == Fn)
 				{
+					forceClear();			// IsFnEnableの前に実施する事が大事
 					IsFnEnable = false;
 					TurnOffStatusLed(2);
-					return 1;
-				}
-				if (IsFnEnable)
-				{
-					Keyboard.release(FnSymbol[row][col]);
 				}
 				else
 				{
-					Keyboard.release(Symbol[row][col]);
+					Keyboard.release(tgt);
 				}
 			}
 		}
 	}
-	return 0;
 }
 
-// Fnキーがリリースされた事によるアクション
-void releaseFuncKeyAction(char PressingKey[][COLMAX], int FnSymbol[][COLMAX])
+// 左右すべての押下中のキーをすべてクリアします
+static void forceClear()
 {
-	// 同時押しされていたキーをすべてリリースします。(Fnキーコードとしてリリースを発射します。)
 	for (int row = 0; row < ROWMAX; row++)
 	{
 		for (int col = 0; col < COLMAX; col++)
 		{
-			if (PressingKey[row][col] == ON)
+			if (OldRKey[row][col] == ON)
 			{
-				Keyboard.release(FnSymbol[row][col]);
+				Keyboard.release(getSymbol(row, col, Right, IsFnEnable));
+			}
+			if (OldLKey[row][col] == ON)
+			{
+				Keyboard.release(getSymbol(row, col, Left, IsFnEnable));
 			}
 		}
 	}
+	memset(OldRKey, (char)OFF, SUM);
+	memset(OldLKey, (char)OFF, SUM);
 }
 
+// キーが今越されたかを判定する
+static bool isTurnOn(int row, int col, LorR lr)
+{
+	bool ret = false;
+	if (lr == Left)
+	{
+		ret = (OldLKey[row][col] == OFF) && (LKey[row][col] == ON);
+	}
+	else
+	{
+		ret = (OldRKey[row][col] == OFF) && (RKey[row][col] == ON);
+	}
+	return ret;
+}
 
+// キーが今離されたかを判定する
+static bool isTurnOff(int row, int col, LorR lr)
+{
+	bool ret = false;
+	if (lr == Left)
+	{
+		ret = (OldLKey[row][col] == ON) && (LKey[row][col] == OFF);
+	}
+	else
+	{
+		ret = (OldRKey[row][col] == ON) && (RKey[row][col] == OFF);
+	}
+	return ret;
+}
+
+// キーシンボルを取得
+static int getSymbol(int row, int col, LorR lr, bool isFn)
+{
+	int ret = 0;
+	if (lr == Left)
+	{
+		if (!isFn)
+		{
+			ret = LSymbol[row][col];
+		}
+		else
+		{
+			ret = LFnSymbol[row][col];
+		}
+
+	}
+	else
+	{
+		if (!isFn)
+		{
+			ret = RSymbol[row][col];
+		}
+		else
+		{
+			ret = RFnSymbol[row][col];
+		}
+	}
+	return ret;
+}
 
 
 
